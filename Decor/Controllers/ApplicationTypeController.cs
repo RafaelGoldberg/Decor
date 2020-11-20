21ea336760a7f8db2fe751634e4c7264a1d08996
Rafael Goldberg <Rafaelgoldberg@outlook.com>
2020-11-20 12:41:00 -0500

    Initial Commit - Category, complete - Product, In Progress

---
+++ Decor/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,126 @@
+﻿using Decor.Data;
+using Decor.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Decor.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objList = _db.ApplicationType;
+            return View(objList);
+        }
+
+
+        //Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        //Post Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+
+        //Get Edit
+        public IActionResult Edit(int? Id)
+        {
+
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.ApplicationType.Find(Id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+
+        //Post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+
+
+        //Get Delete
+        public IActionResult Delete(int? Id)
+        {
+
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.ApplicationType.Find(Id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+
+        //Post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.ApplicationType.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+
+    }
+}
---
+++ Decor/Controllers/ProductContoller.cs
@@ -0,0 +1,132 @@
+﻿using Decor.Data;
+using Decor.Models;
+using Decor.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Decor.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = _db.Product;
+
+            foreach (var obj in objList)
+            {
+                obj.Category = _db.Category.FirstOrDefault(u => u.Id == obj.CategoryId);
+            }
+            return View(objList);
+        }
+
+        //Get Upsert
+        public IActionResult Upsert(int? id)
+        {
+            //IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(i => new SelectListItem
+            //{
+            //    Text = i.Name,
+            //    Value = i.Id.ToString()
+            //});
+
+            //ViewBag.CategoryDropDown = CategoryDropDown;
+
+            //Product product = new Product();
+
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategorySelectList = _db.Category.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+
+            };
+            if (id == null)
+            {
+                //This is for create
+                return View(productVM);
+            }
+            else
+            {
+                productVM.Product = _db.Product.Find(id);
+                if (productVM.Product == null)
+                {
+                    return NotFound();
+                }
+
+
+
+            }
+            return View(productVM);
+        }
+
+
+
+        //Post Upsert
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+         
+        }
+
+
+
+        //Get Delete
+        public IActionResult Delete(int? Id)
+        {
+
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(Id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+
+        //Post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound(); 
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+
+    }
+}
